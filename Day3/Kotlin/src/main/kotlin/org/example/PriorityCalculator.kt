981184db0f8c441ad37276215d8437c835aee455
David <david@Davids-MacBook-Pro.local>
2022-12-03 06:26:37 +0000

    Day 3 - Kotlin cleanup

--- Day3/Kotlin/src/main/kotlin/org/example/PriorityCalculator.kt
+++ Day3/Kotlin/src/main/kotlin/org/example/PriorityCalculator.kt
@@ -4,40 +4,43 @@ import java.io.BufferedReader
 
 class PriorityCalculator(private val reader: BufferedReader) {
     fun calculate() =
-        reader.useLines { lines ->
+        reader.useLines { lines: Sequence<String> ->
             lines
-                .map { line ->
-                    val (left, right) = Pair(line.take(line.length/2), line.takeLast(line.length/2))
-                    val union = left.toCharArray().intersect(right.toCharArray().asIterable())
-//                    println("union: $union")
-//                    println("code: ${union.first().code}/${union.first().code.priority()}")
-                    union.sumOf { it.code.priority() }
-                }
-                .sum()
+                .map(String::halvsies)
+                .map(Pair<String,String>::mapToSet)
+                .map(Pair<Set<Char>,Set<Char>>::reduceIntersect)
+                .map(Set<Char>::first)
+                .sumOf(Char::priority)
         }
 
     fun calculateBadges() =
         reader.useLines { lines ->
-            lines.fold(mutableListOf<MutableList<String>>(mutableListOf())) { groups, line ->
-                if (groups.last().size >= 3) {
-                    groups.add(mutableListOf())
-                }
-                groups.last().add(line)
-                groups
-            }
-                .map { groupLines ->
-                    groupLines.takeLast(2).fold(groupLines.first().toSet()) { sect, line ->
-                        sect.intersect(line.toCharArray().asIterable())
-                    }
-                    .sumOf { it.code.priority() }
+            lines
+                .map(String::toSet)
+                .windowed(3, 3) { groupLines ->
+                    groupLines
+                        .reduce { first, second -> first.intersect(second) }
+                        .sumOf { it.priority() }
                 }
                 .sum()
         }
 }
 
-private fun Int.priority() =
-    when (this) {
-        in 65..90 -> this - 38
-        in 97..122 -> this - 96
-        else -> this
+private fun String.halvsies(): Pair<String, String> =
+    Pair(
+        this.take(this.length / 2),
+        this.takeLast(this.length / 2),
+    )
+
+private fun Pair<String, String>.mapToSet(): Pair<Set<Char>,Set<Char>> =
+    Pair(this.first.toSet(), this.second.toSet())
+
+private fun Pair<Set<Char>, Set<Char>>.reduceIntersect() =
+    this.first.intersect(this.second)
+
+private fun Char.priority() =
+    when (this.code) {
+        in 65..90 -> this.code - 38
+        in 97..122 -> this.code - 96
+        else -> 0
     }
