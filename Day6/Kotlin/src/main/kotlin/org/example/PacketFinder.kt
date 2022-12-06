c48163ca445969d082b7207bbafe9d9cf48a35df
David <david@Davids-MacBook-Pro.local>
2022-12-05 22:45:21 -0700

    Day 6 - Kotlin cleanup

--- Day6/Kotlin/src/main/kotlin/org/example/PacketFinder.kt
+++ Day6/Kotlin/src/main/kotlin/org/example/PacketFinder.kt
@@ -1,31 +1,14 @@
 package org.example
 
 class PacketFinder(private val dataStream: String) {
-    fun findStartPacket(): Int {
-        var foundIndex = -1
-        dataStream
-            .asIterable()
-            .forEachIndexed { index, _ ->
-                if (foundIndex >= 0) return@forEachIndexed
-                if ((index + 4) > dataStream.length) return@forEachIndexed
-                if (dataStream.substring(index, index + 4).toSet().count() == 4) {
-                    foundIndex = index
-                }
-            }
-        return foundIndex + 4
-    }
+    fun findStartPacket() = findDatagram(4)
+    fun findMessagePacket() = findDatagram(14)
 
-    fun findMessagePacket(): Int {
-        var foundIndex = -1
-        dataStream
-            .asIterable()
-            .forEachIndexed { index, _ ->
-                if (foundIndex >= 0) return@forEachIndexed
-                if ((index + 14) > dataStream.length) return@forEachIndexed
-                if (dataStream.substring(index, index + 14).toSet().count() == 14) {
-                    foundIndex = index
-                }
-            }
-        return foundIndex + 14
+    private fun findDatagram(gramLength: Int): Int {
+        for (index in 1..dataStream.length) {
+            if ((index + gramLength) > dataStream.length) break
+            if (dataStream.substring(index, index + gramLength).toSet().count() == gramLength) return index + gramLength
+        }
+        return -1
     }
 }
